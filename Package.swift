c5cacaa1a564930425242116b3270f50b14eaf6a
Kevin Kieffer <kdkieffer@icloud.com>
2020-01-04 10:30:17 -0500

    Use my BlueSSLFork, using master branch (tip)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSocket.git", from: "1.0.0"),
-        .package(url: "https://github.com/kkieffer/BlueSSLService.git", from: "1.0.0")
+        .package(url: "https://github.com/kkieffer/BlueSSLService.git", .branch("master"))
     ],
     targets: [
         .target(
